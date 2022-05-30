8b01c08eb284197c9c42f318cb5974d881acbe30
Arcayn <86525700+Arcaynx@users.noreply.github.com>
2022-05-31 00:07:48 +0200

    Added possible performance improvement to zigzag

--- encoder.py
+++ encoder.py
@@ -4,6 +4,7 @@ from scipy.fftpack import dct
 import quantization as quant
 
 
+
 def encoder(yuv_vid):
     '''
         Encodes the video and returns a bitstream
@@ -70,32 +71,49 @@ def zigzag(blocks):
 
     totalSize = blocks["frames"] * (blocks["luma_frame_size"][0] * blocks["luma_frame_size"][1] + blocks["chroma_frame_size"][0] * blocks["chroma_frame_size"][1] * 2)
     bitstream = np.empty(totalSize)
-    i = 0
+    cursor = 0
+
+    zigzagIndices = {}
 
     for component in ["Y", "U", "V"]:
         
         for key in blocks[component]:
 
             blockSize = blocks[component][key].shape
+            pixelCount = blockSize[0] * blockSize[1]
 
-            #Horizontal case
-            for x in range(blockSize[0] - 1):
-                curx = x
-                cury = 0
-                while curx >= 0 and cury < blockSize[1]:
-                    bitstream[i] = blocks[component][key][curx, cury]
-                    i += 1
-                    curx -= 1
-                    cury += 1
+            if blockSize not in zigzagIndices:
 
-            #Vertical case
-            for y in range(blockSize[1]):
-                cury = y
-                curx = blockSize[0] - 1
-                while curx >= 0 and cury < blockSize[1]:
-                    bitstream[i] = blocks[component][key][curx, cury]
-                    i += 1
-                    curx -= 1
-                    cury += 1
+                transformArray = np.empty(pixelCount)
+                i = 0
+                
+                #Horizontal case
+                for x in range(blockSize[0] - 1):
+                    curx = x
+                    cury = 0
+                    while curx >= 0 and cury < blockSize[1]:
+                        transformArray[curx * blockSize[0] + curY] = i
+                        i += 1
+                        curx -= 1
+                        cury += 1
+
+                #Vertical case
+                for y in range(blockSize[1]):
+                    cury = y
+                    curx = blockSize[0] - 1
+                    while curx >= 0 and cury < blockSize[1]:
+                        transformArray[curx * blockSize[0] + curY] = i
+                        i += 1
+                        curx -= 1
+                        cury += 1
+
+                zigzagIndices[blockSize] = transformArray
+
+            for n in range(pixelCount):
+
+                index = blockSizezigzagIndices[blockSize][n];
+                bitstream[cursor + n] = blocks[component][key][index % blockSize[0], index // blockSize[0]]
+
+            cursor += pixelCount
 
     return bitstream
